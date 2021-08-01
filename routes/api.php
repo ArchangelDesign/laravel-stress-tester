45f37662387a146653855ca4388f0f84fe37b5ad
Raff-WIndows <archangel.raffael@gmail.com>
2021-08-01 17:54:06 -0400

    stress testing endpoints and cleanup

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,5 @@
 Route::get('/stress/alive', ['uses' => 'StressTest\Controllers\StressTestController@alive']);
 Route::get('/stress/db-connected', ['uses' => 'StressTest\Controllers\StressTestController@isDbConnected']);
 Route::get('/stress/open-db-connections', ['uses' => 'StressTest\Controllers\StressTestController@getOpenDbConnections']);
+Route::post('/stress/close-db-connection', ['uses' => 'StressTest\Controllers\StressTestController@closeDbConnection']);
+Route::post('/stress/db/eloquent/fetch-all', ['uses' => 'StressTest\Controllers\StressTestController@eloquentFetchAll']);
--- src/Controllers/StressTestController.php
+++ src/Controllers/StressTestController.php
@@ -3,6 +3,8 @@
 namespace StressTest\Controllers;
 
 use App\Http\Controllers\Controller;
+use http\Env\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 /**
@@ -36,12 +38,34 @@ class StressTestController extends Controller
         });
     }
 
+    public function closeDbConnection()
+    {
+        return $this->issueResponse(function() {
+            return DB::disconnect();
+        });
+    }
+
+    public function eloquentFetchAll(Request $request)
+    {
+        return $this->issueResponse(function() use ($request) {
+            $model = $request->post('entity');
+            $namespace = 'App\Models\\' . $model;
+            if (!class_exists($namespace))
+                return response()->json(['error' => 'Model ' . $namespace . ' not found.'], 404);
+            DB::enableQueryLog();
+            $namespace::all();
+            DB::getQueryLog();
+
+            return response()->json(['log' => DB::getQueryLog()]);
+        });
+    }
+
     private function issueResponse(callable $callback)
     {
         try {
             return response()->json($callback());
         } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
+            return response()->json(['error' => $e->getMessage(), 'cwd' => getcwd()], 500);
         }
     }
 }
